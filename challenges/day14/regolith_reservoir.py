417be877ab72c1747f3b3244db2954c316b027b0
Michael Witkovsky <michael.witkovsky@gmail.com>
2022-12-20 11:46:47 -0500

    day 14

--- challenges/day14/regolith_reservoir.py
+++ challenges/day14/regolith_reservoir.py
@@ -1,4 +1,5 @@
 # Path hack
+from io import TextIOWrapper
 import os
 import sys
 sys.path.insert(0, os.path.abspath("../../"))  # noqa
@@ -12,6 +13,44 @@ class TileType:
     SAND = 1
     ROCK = 2
     SOURCE = 3
+    SAND_REST = 4
+
+
+class SandGrain:
+    def __init__(self, x: int, y: int):
+        self.__x = x
+        self.__y = y
+        self.__at_rest = False
+
+    def move(self, map: dict[int, dict[int, int]],
+             floor_y: int = None) -> None:
+        if floor_y is not None and self.__y+1 == floor_y:
+            self.__at_rest = True
+            return
+
+        target_row = map[self.__y + 1]
+        if target_row[self.__x] == TileType.AIR:
+            self.__y += 1
+        elif target_row[self.__x-1] == TileType.AIR:
+            self.__x -= 1
+            self.__y += 1
+        elif target_row[self.__x+1] == TileType.AIR:
+            self.__x += 1
+            self.__y += 1
+        else:
+            self.__at_rest = True
+
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
+
+    @property
+    def at_rest(self):
+        return self.__at_rest
 
 
 class Reservoir:
@@ -20,10 +59,11 @@ class Reservoir:
         return tuple(map(int, vertex_desc.split(",")))
 
     def __record_tile(self, x: int, y: int, tile_type: int) -> None:
+        if tile_type in (TileType.SOURCE, TileType.ROCK):
+            self.__min_y = min(self.__min_y, y)
+            self.__max_y = max(self.__max_y, y)
         self.__min_x = min(self.__min_x, x)
         self.__max_x = max(self.__max_x, x)
-        self.__min_y = min(self.__min_y, y)
-        self.__max_y = max(self.__max_y, y)
         self.__map[y][x] = tile_type
 
     def __record_rock_path(self, pos1: tuple[int, int], pos2: tuple[int, int]):
@@ -55,6 +95,9 @@ class Reservoir:
             )
         )  # type: dict[int, dict[int, int]]
         self.__source_pos = source_pos
+        self.__active_sand = []  # type: list[SandGrain]
+        self.__spawner_delay = False
+        self.__num_particles_spawned = 0
         self.__record_tile(source_pos[0], source_pos[1], TileType.SOURCE)
         for rock_path_desc in rock_path_descs:
             rock_path_vertexes = rock_path_desc.split(" -> ")
@@ -67,20 +110,89 @@ class Reservoir:
                 )
                 self.__record_rock_path(v1, v2)
 
-    def render_map_to_file(self, fn: str = "out.txt") -> None:
-        with open(fn, "w") as f:
-            for y in range(self.__min_y, self.__max_y+1):
-                for x in range(self.__min_x, self.__max_x+1):
-                    tile = self.__map[y][x]
-                    if tile == TileType.AIR:
-                        f.write(" ")
-                    elif tile == TileType.SAND:
-                        f.write("*")
-                    elif tile == TileType.ROCK:
-                        f.write("#")
-                    elif tile == TileType.SOURCE:
-                        f.write("V")
-                f.write("\n")
+    def __try_spawn_sand(self) -> bool:
+        if self.__spawner_delay:
+            self.__spawner_delay = False
+            return
+
+        spawner_tile = self.__map[self.__source_pos[1]][self.__source_pos[0]]
+        if spawner_tile == TileType.SOURCE:
+            self.__active_sand.append(
+                SandGrain(self.__source_pos[0], self.__source_pos[1])
+            )
+            self.__record_tile(
+                self.__source_pos[0], self.__source_pos[1], TileType.SAND
+            )
+            self.__spawner_delay = True
+            self.__num_particles_spawned += 1
+            return True
+        return False
+
+    def __move_sand(self, floor_exists: bool = False) -> None:
+        grains_to_remove = []
+        for grain in self.__active_sand:
+            if (grain.x, grain.y) == self.__source_pos:
+                before_tile_type = TileType.SOURCE
+            else:
+                before_tile_type = TileType.AIR
+            self.__record_tile(grain.x, grain.y, before_tile_type)
+
+            floor_y = None
+            if floor_exists:
+                floor_y = self.__max_y + 2
+            grain.move(self.__map, floor_y=floor_y)
+
+            after_pos = (grain.x, grain.y)
+            if grain.at_rest:
+                grains_to_remove.append(grain)
+                after_tile_type = TileType.SAND_REST
+            else:
+                after_tile_type = TileType.SAND
+
+            self.__record_tile(after_pos[0], after_pos[1], after_tile_type)
+
+        for grain in grains_to_remove:
+            self.__active_sand.remove(grain)
+
+    def simulate_sand_num_particles(self, num_particles: int) -> None:
+        while num_particles > 0 or len(self.__active_sand) != 0:
+            if num_particles > 0:
+                num_particles -= self.__try_spawn_sand()
+            self.__move_sand()
+
+    def simulate_sand_until_covered(self) -> None:
+        while self.__map[self.__source_pos[1]][self.__source_pos[0]] != \
+                TileType.SAND_REST:
+            self.__try_spawn_sand()
+            self.__move_sand(floor_exists=True)
+
+    def render_map_to_file(self, fn: str = None) -> None:
+        if fn:
+            out = open(fn, "w")
+        else:
+            out = sys.stdout
+
+        for y in range(self.__min_y, self.__max_y+1):
+            for x in range(self.__min_x, self.__max_x+1):
+                tile = self.__map[y][x]
+                if tile == TileType.AIR:
+                    out.write(" ")
+                elif tile == TileType.SAND:
+                    out.write("*")
+                elif tile == TileType.SAND_REST:
+                    out.write("O")
+                elif tile == TileType.ROCK:
+                    out.write("#")
+                elif tile == TileType.SOURCE:
+                    out.write("V")
+            out.write("\n")
+
+        if fn is not None:
+            out.close()
+
+    @property
+    def num_particles_spawned(self):
+        return self.__num_particles_spawned
 
 
 def _regolith_reservoir(inp: list[str]):
@@ -89,11 +201,15 @@ def _regolith_reservoir(inp: list[str]):
         Using your scan, simulate the falling sand. How many units of sand
         come to rest before sand starts flowing into the abyss below?
     Part 2:
-
+        Using your scan, simulate the falling sand until the source of the 
+        sand becomes blocked. How many units of sand come to rest?
     """
     reservoir = Reservoir(inp, (500, 0))
-    reservoir.render_map_to_file()
-    pass
+    # reservoir.simulate_sand_num_particles(1513)
+    # reservoir.render_map_to_file("out.txt")
+    reservoir.simulate_sand_until_covered()
+    reservoir.render_map_to_file("out.txt")
+    print(f"Num particles spawned: {reservoir.num_particles_spawned}")
 
 
 if __name__ == "__main__":
